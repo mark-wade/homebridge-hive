c7b73a0f9cbb7316022b82b94c85d6632fbc5fc3
James Fowler <keith@difficult.me.uk>
2017-09-07 10:49:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ HiveThermostat.prototype = {
 			for ( var i = 0; i < body.nodes.length; i++ ) {
 				if ( body.nodes[i].nodeType == "http:\/\/alertme.com\/schema\/json\/node.class.thermostat.json#" && body.nodes[i].attributes.temperature && ( !this.id || body.nodes[i].id == this.id ) ) {
 					this.cachedMainData = body.nodes[i];
-                                        this.log(JSON.stringify(body.nodes[i]);
+                                        this.log(JSON.stringify(body.nodes[i]));
 					if ( showIds ) {
 						this.log("Found thermostat " + body.nodes[i].id + ". Current temperature is " + body.nodes[i].attributes.temperature.reportedValue + ", set to " + body.nodes[i].attributes.targetHeatTemperature.reportedValue );
 					}
